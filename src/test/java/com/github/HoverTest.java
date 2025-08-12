3b9464b4465c4c2fa3372b8a67d151daea063a67
NJcherry <inessaogarkova@mail.ru>
2025-08-12 16:59:31 +0300

    added configuration in HoverTest

--- src/test/java/com/github/HoverTest.java
+++ src/test/java/com/github/HoverTest.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.UUID;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -14,7 +17,16 @@ public class HoverTest {
     static void configure() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.headless = true;
+        String userDataDir = "/tmp/chrome-profile-" + UUID.randomUUID();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--headless",
+                "--disable-dev-shm-usage",
+                "--no-sandbox",
+                "--window-size=1920,1080",
+                "--user-data-dir=" + userDataDir
+        );
     }
 
     @Test
