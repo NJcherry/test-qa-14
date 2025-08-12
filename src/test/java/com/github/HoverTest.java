807bbe2d846bacc868284fe8781ef356e3207739
NJcherry <inessaogarkova@mail.ru>
2025-08-12 16:47:27 +0300

    added configuration in HoverTest

--- src/test/java/com/github/HoverTest.java
+++ src/test/java/com/github/HoverTest.java
@@ -14,7 +14,7 @@ public class HoverTest {
     static void configure() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 8000;
+        Configuration.headless = true;
     }
 
     @Test
