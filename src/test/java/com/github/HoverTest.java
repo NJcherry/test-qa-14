5cf8523eba22ee803af93061a1e1d7bd74689f79
NJcherry <inessaogarkova@mail.ru>
2025-08-12 21:06:46 +0300

    fixed HoverTest

--- src/test/java/com/github/HoverTest.java
+++ src/test/java/com/github/HoverTest.java
@@ -1,33 +1,14 @@
 package com.github;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import com.demoqa.config.TestBase;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.UUID;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class HoverTest {
-    @BeforeAll
-    static void configure() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
-        String userDataDir = "/tmp/chrome-profile-" + UUID.randomUUID();
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments(
-                "--headless",
-                "--disable-dev-shm-usage",
-                "--no-sandbox",
-                "--window-size=1920,1080",
-                "--user-data-dir=" + userDataDir
-        );
-    }
+public class HoverTest extends TestBase {
 
     @Test
     @Tag("github")
@@ -38,5 +19,6 @@ public class HoverTest {
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
 
        // sleep(30_000);
+
     }
 }
